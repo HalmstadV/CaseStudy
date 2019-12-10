db7b2a49279e947e818a84d9f633e87b9b9987c7
HalmstadV <pikdokenaral@gmail.com>
2019-12-10 03:32:05 +0300

    desired capabilities

--- src/test/java/constants/DeviceCapabilites.java
+++ src/test/java/constants/DeviceCapabilites.java
@@ -14,6 +14,7 @@ public class DeviceCapabilites {
         PIXEL_2_CAPABILITIES.put("appPackage", "com.getir.getirtestingcasestudy");
         PIXEL_2_CAPABILITIES.put("appActivity", "com.getir.getirtestingcasestudy.ui.login.LoginActivity");
         PIXEL_2_CAPABILITIES.put("noReset", "true");
+        PIXEL_2_CAPABILITIES.put("uiautomator2ServerInstallTimeout", 60000);
         PIXEL_2_CAPABILITIES.put("newCommandTimeout", 100000);
     }
 }
